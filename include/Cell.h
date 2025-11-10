54f95e656cd7c646283c7ad8c78fe6591ff766b3
Rob <rsamoraj11@gmail.com>
2025-11-10 14:33:32 -0600

    Add Cell attributes

--- include/Cell.h
+++ include/Cell.h
@@ -1,10 +1,18 @@
 #ifndef CELL_H
 #define CELL_H
 
+
 class Cell
 {
 private:
-    /*data*/
+    int row;
+    int column;
+
+    Cell* north;
+    Cell* east;
+    Cell* south;
+    Cell* west;
+
 
 public:
     Cell(/*args*/);
