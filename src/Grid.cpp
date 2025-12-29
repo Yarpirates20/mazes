19d886a6d5370802a91024d16caf0760c0045d00
Robert Samoraj <rsamoraj11@gmail.com>
2025-12-29 16:04:28 -0600

    Add row and cell iterator functions

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -4,19 +4,19 @@
 /** @copydoc Grid::prepare_grid() */
 void Grid::prepare_grid()
 {
-    for(int r = 0; r < rows; ++r)
+    for (int r = 0; r < rows; ++r)
     {
-        for(int c = 0; c < columns; ++c)
+        for (int c = 0; c < columns; ++c)
         {
             grid[r][c] = new Cell(r, c);
         }
     }
 }
 
-// TODO: Configure cells are creating atrc() and each_cell
 /** @copydoc Grid::configure_cells() */
 void Grid::configure_cells()
 {
+    // TODO: Configure cells are creating atrc() and each_cell
     return;
 }
 
@@ -25,7 +25,7 @@ Cell &Grid::atrc(int r, int c)
 {
     if (r < 0 || r >= rows || c < 0 || c >= columns)
     {
-        throw std::out_of_range("Coordinate row: " + std::to_string(r) + ", column: " + std::to_string(c) + " out of bounds"); 
+        throw std::out_of_range("Coordinate row: " + std::to_string(r) + ", column: " + std::to_string(c) + " out of bounds");
     }
 
     return *grid[r][c];
@@ -34,5 +34,54 @@ Cell &Grid::atrc(int r, int c)
 /** @copydoc Grid::atrc(int r, int c) const */
 Cell &Grid::atrc(int r, int c) const
 {
-    // TODO: insert return statement here
+    if (r < 0 || r >= rows || c < 0 || c >= columns)
+    {
+        throw std::out_of_range("Coordinate row: " + std::to_string(r) + ", column: " + std::to_string(c) + " out of bounds");
+    }
+
+    return *grid[r][c];
+}
+
+/** @copydoc Grid::get_size() */
+int Grid::get_size()
+{
+    return rows * columns;
+}
+
+/** @copydoc Grid::for_each_row(FUNC &&fn) */
+template <typename FUNC>
+void Grid::for_each_row(FUNC &&fn)
+{
+    for (auto &row : grid)
+        fn(row);
+}
+
+/** @copydoc Grid::for_each_row(FUNC &&fn) const */
+template <typename FUNC>
+void Grid::for_each_row(FUNC &&fn) const
+{
+    for (auto &row : grid)
+        fn(row);
+}
+
+/** @copydoc Grid::for_each_cell(FUNC &&fn) */
+template <typename FUNC>
+void Grid::for_each_cell(FUNC &&fn)
+{
+    for_each_row([&fn](auto &row)
+                 {
+        for (Cell *c : row)
+            if (c) 
+                fn(*c); });
+}
+
+/** @copydoc Grid::for_each_cell(FUNC &&fn) const */
+template <typename FUNC>
+void Grid::for_each_cell(FUNC &&fn) const
+{
+    for_each_row([&fn](auto const &row)
+                 {
+        for (Cell const *c : row)
+            if (c) 
+                fn(*c); });
 }
--- src/include/Grid.h
+++ src/include/Grid.h
@@ -19,7 +19,7 @@ private:
 public:
     /**
      * @brief Constructor.
-     * 
+     *
      * @param r Number of rows.
      * @param c Number of columns.
      */
@@ -27,36 +27,87 @@ public:
 
     /**
      * @brief Prepares a 2D vector of Cell instances.
-     * 
+     *
      */
     void prepare_grid();
-    
+
     /**
-     * @brief Configures cell neighbors. 
-     * 
+     * @brief Configures cell neighbors.
+     *
      */
     void configure_cells();
 
     /**
-     * @brief Accessor with bounds checking for grid coordinates using row, 
+     * @brief Accessor with bounds checking for grid coordinates using row,
      * columns.
-     * 
-     * @param r Row. 
+     *
+     * @param r Row.
      * @param c Column.
      * @return Cell& Returns modifiable reference to Cell at r, c.
      */
     Cell &atrc(int r, int c);
 
     /**
-     * @brief Const accessor with bounds checking for grid coordinates using 
+     * @brief Const accessor with bounds checking for grid coordinates using
      * row, columns.
-     * 
-     * @param r Row. 
+     *
+     * @param r Row.
      * @param c Column.
      * @return Cell& Returns const reference to Cell at r, c.
      */
     Cell &atrc(int r, int c) const;
-    
+
+    /**
+     * @brief Get the size of the grid.
+     *
+     * @return int Number of Cell that Grid contains.
+     */
+    int get_size();
+
+    /**
+     * @brief Accesses random cell.
+     *
+     * @return Cell& Reference to chosen Cell.
+     */
+    Cell &random_cell();
+
+    /**
+     * @brief Template function that calls fn(row) for each row.
+     *
+     * @tparam FUNC Callable type.
+     * @param fn Function to call.
+     */
+    template <typename FUNC>
+    void for_each_row(FUNC &&fn);
+
+    /**
+     * @brief Const template function that calls fn(row) for each row.
+     *
+     * @tparam FUNC Callable type.
+     * @param fn Function to call.
+     */
+    template <typename FUNC>
+    void for_each_row(FUNC &&fn) const;
+
+    /**
+     * @brief Template function that calls fn(cell) for each cell in each
+     * row in Grid.
+     *
+     * @tparam FUNC Callable type.
+     * @param fn Function to call.
+     */
+    template <typename FUNC>
+    void for_each_cell(FUNC &&fn);
+
+    /**
+     * @brief Const template function that calls fn(cell) for each cell
+     * in each row in Grid.
+     *
+     * @tparam FUNC Callable type.
+     * @param fn Function to call.
+     */
+    template <typename FUNC>
+    void for_each_cell(FUNC &&fn) const;
 };
 
 #endif // GRID_H
